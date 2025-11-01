11402cc6a37ab7ac05d586df7e4b612ff544962f
BIKESHOPROMEO <tama@realeyes.co.jp>
2025-11-01 18:43:50 +0900

    Add files via upload

--- api/holiday.js
+++ api/holiday.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch(`${GAS_URL}?action=holidays`);
+    const response = await fetch(`${GAS_URL}?action=holidaystest`);
     const text = await response.text();
 
     try {
